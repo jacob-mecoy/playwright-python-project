c8a490aa8c213f8ce4e52b2599721174de45852e
jacob-mecoy <jacob.mecoy@gmail.com>
2025-07-05 16:12:59 +0100

    abstract api actions to RickAndMortyAPI class

--- step_defs/RickAndMorty/api_steps.py
+++ step_defs/RickAndMorty/api_steps.py
@@ -4,21 +4,21 @@ from playwright.sync_api import Page, expect
 
 from pages.RickAndMorty.api import RickAndMortyAPI
 
-@pytest.fixture()
-def api() -> RickAndMortyAPI:
-    return RickAndMortyAPI()
+@pytest.fixture
+def api(page: Page) -> RickAndMortyAPI:
+    return RickAndMortyAPI(page)
 
 @given("I get all characters using the character API")
-def get_all_characters(api: RickAndMortyAPI, page: Page):
-    api.response = page.request.get("https://rickandmortyapi.com/api/character")
+def get_all_characters(api: RickAndMortyAPI):
+    api.get_all_characters()
 
 @given("I get a single character via id")
-def get_one_character(api: RickAndMortyAPI, page: Page):
-    api.response = page.request.get("https://rickandmortyapi.com/api/character/1")
+def get_one_character(api: RickAndMortyAPI):
+    api.get_character_by_id(1)
 
 @given("I get characters that match specific filters")
-def get_characters_using_filters(api: RickAndMortyAPI, page: Page):
-    api.response = page.request.get("https://rickandmortyapi.com/api/character", params={"name":"rick", "status":"alive"})
+def get_characters_using_filters(api: RickAndMortyAPI):
+    api.get_characters_with_params({"name":"rick", "status":"alive"})
 
 @then("the response is ok")
 def verify_response_is_ok(api: RickAndMortyAPI):
@@ -26,12 +26,16 @@ def verify_response_is_ok(api: RickAndMortyAPI):
 
 @then(parsers.parse("the response contains only {number_of_characters:d} character"))
 @then(parsers.parse("the response contains {number_of_characters:d} characters"))
-def verify_response_contains_characters(api: RickAndMortyAPI, number_of_characters: int):
+def verify_character_count(api: RickAndMortyAPI, number_of_characters: int):
+    """
+    Verifies that the API response contains the expected number of characters.
+    - For single characters, expects a dict that doesn't contain 'info'.
+    - For multiple characters, expects a paginated response with 'info.count'.
+    """
+    response_json = api.response.json()
     if number_of_characters == 1:
-        #verify that multiple characters are not returned by verifying a list is not returned
-        assert type(api.response.json()) == dict
-        #verify that one characters is returned by verifying the "info" key is not present in the response
-        assert "info" not in api.response.json().keys()
+        assert isinstance(response_json, dict)
+        assert "info" not in response_json.keys()
     else:
-        character_count = api.response.json().get("info").get("count")
-        assert character_count == number_of_characters
+        response_character_count = response_json.get("info", {}).get("count")
+        assert response_character_count == number_of_characters
