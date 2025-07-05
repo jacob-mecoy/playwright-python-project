51ecd4a267c80e33188f4b9c76afcabfbeeddc19
jacob-mecoy <jacob.mecoy@gmail.com>
2025-07-05 16:34:09 +0100

    move api tests to dedicated file, update readme for BDD

--- tests/test_rick_and_morty.py
+++ tests/test_rick_and_morty.py
@@ -3,6 +3,5 @@ from pytest_bdd import scenarios
 # Import step definitions so pytest can register them
 from step_defs.RickAndMorty.character_steps import *
 from step_defs.RickAndMorty.navigation_steps import *
-from step_defs.RickAndMorty.api_steps import *
 
-scenarios("../features/RickAndMorty")
+scenarios("../features/RickAndMorty/rickandmorty_ui.feature")
